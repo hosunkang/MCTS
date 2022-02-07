26908a582c2a805d8af1d3061304e06ee1086b0e
hosunkang <khsuniv201@gmail.com>
2022-02-07 15:17:02 +0900

    first

---
+++ mcts.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+def make_ground():
+    bg = np.zeros((1000,1000,3),np.uinit8)
+    return bg
+
+class TreeSearch:
+    def __init__(self):
+        pass
+    def mtcs(self):
+        print('Start MTCS')
+        bg = np.full((800,1500,3),255,np.uint8)
+        cv2.imshow('Window', bg)
+        cv2.waitKey(0)
+
+if __name__ == "__main__":
+    TS = TreeSearch()
+    TS.mtcs()
+    cv2.destroyAllWindows()
+
