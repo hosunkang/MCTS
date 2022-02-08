337a78ce3aa7f5a00b601a43f4ac264efe684d51
hosunkang <khsuniv201@gmail.com>
2022-02-08 14:24:20 +0900

    QT window update

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 from email.charset import QP
-import sys
+import sys, random
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QPainter
-from PyQt5.QtCore import Qt, QPoint, QRect
+from PyQt5.QtCore import Qt
 
 form_class = uic.loadUiType("ui_treesearch.ui")[0]
 
@@ -14,24 +14,28 @@ class MyWindow(QMainWindow, form_class):
         self.bt_draw_planar.clicked.connect(self.bt_draw_clicked)
         self.bt_mcts.clicked.connect(self.bt_mcts_clicked)
 
-        self.pix = QPixmap(self.rect().size())
-        self.pix.fill(Qt.white)
-
-        self.begin, self.destination = QPoint(), QPoint()
-    
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawPixmap(QPoint(), self.pix)
-
-        if not self.begin.isNull() and not self.destination.isNull():
-            rect = QRect(self.begin, self.destination)
-            painter.drawRect(rect.normalized())
-        
-    def bt_draw_clicked(self):
+    def bt_draw_clicked(self, event):
         print("Click Draw planar button")
+        self.pix = QPixmap(self.draw_label.size()) #size(1021,501)
+        self.pix.fill(Qt.transparent)
+        qp = QPainter(self.pix)
+
+        # Start
+        qp.setBrush(Qt.green)
+        qp.drawRect(0, 250, 20, 20)
+        # Goal
+        qp.setBrush(Qt.red)
+        qp.drawRect(1000, 250, 20, 20)
+
+        qp.setBrush(Qt.SolidPattern)
+        for i in range(100):
+            w = random.randrange(0, 1000)
+            h = random.randrange(0, 450)
+            qp.drawRect(w, h, 20, 20)
+        self.draw_label.setPixmap(self.pix)
+
     def bt_mcts_clicked(self):
         print("Click MCTS button")
-    
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
